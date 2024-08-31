d9f4f9f6abfd0e1cd16deb9d861747b38967ddb4
AndreLoppnow <andref_lloppnow@outlook.com>
2024-08-31 08:17:34 -0300

    Initial commit

---
+++ aleatório/MediaUm.py
@@ -0,0 +1,17 @@
+#média ponderada
+A = input()
+B = input()
+Apeso = 3.5
+Bpeso = 7.5
+media = (float(A)*Apeso+float(B)*Bpeso)/(3.5+7.5);
+print("MEDIA = {:.5f}".format(media))
+
+#média ponderada
+A = input()
+B = input()
+C = input()
+Apeso = 2
+Bpeso = 3
+Cpeso = 5
+media = (float(A)*Apeso+float(B)*Bpeso+float(C)*Cpeso)/(Apeso+Bpeso+Cpeso);
+print("MEDIA = {:.1f}".format(media))
---
+++ aleatório/testandoPycharm.py
@@ -0,0 +1,5 @@
+pi = 3.14159
+raio = input()
+raio = float(raio)
+area = float(pi*(raio*raio))
+print("A={:.4f}".format(area))
